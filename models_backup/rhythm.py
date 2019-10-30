eede1c1b86beb9eb2beb8c63c31308600a6b601c
Thomas Graeff <676@holbertonschool.com>
2019-10-30 06:33:48 +0000

    Delete models_backup

--- models_backup/rhythm.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'''Module for Tuning class'''
-from models.base_model import BaseModel
-
-
-class Tuning(BaseModel):
-    '''Inherit from BaseModel'''
-    lesson_id = ''
-    name = ''
