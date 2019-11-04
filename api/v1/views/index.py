f386731e7bacfe6dcb8dc6f6b1c62c5a9fd8958f
Thomas Graeff <676@holbertonschool.com>
2019-11-04 07:12:41 +0000

    Remove unecessary files

--- api/v1/views/index.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-from flask import Flask, jsonify
-from api.v1.views import app_views
-
-
-@app_views.route('/status', methods=['GET'])
-def status():
-    if request.method == 'GET':
-        return jsonify({'status':'OK'}}
