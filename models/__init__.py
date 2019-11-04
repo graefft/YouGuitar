9d36757f7a69edd3f8637bf7c9164a22bf2b52c4
Thomas Graeff <676@holbertonschool.com>
2019-11-04 07:13:17 +0000

    Removes unnecessary files

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-'''creates instance of FileStorage'''
-from models.base_model import BaseModel
-from models.engine.storage import Storage
-from models.lesson import Lesson
-
-storage = Storage()
-storage.reload()
